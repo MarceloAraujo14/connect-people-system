1db8a6eb127051ab04825585f601ec6adceaf4fc
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-12-22 20:46:08 -0300

    develop - refact: (devops): configuring Dockerfile

--- src/main/java/br/com/connectpeople/ConnectPeopleApplication.java
+++ src/main/java/br/com/connectpeople/ConnectPeopleApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class ConnectpeopleApplication {
+public class ConnectPeopleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConnectpeopleApplication.class, args);
+        SpringApplication.run(ConnectPeopleApplication.class, args);
     }
 
 }
