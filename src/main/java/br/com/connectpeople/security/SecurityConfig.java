ff2cd7d413c427030f05218803cc608c0d640f7c
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-12-29 17:25:12 -0300

    T-003 - refact: (security): remove the sign with linkedin from hell

--- src/main/java/br/com/connectpeople/security/SecurityConfig.java
+++ src/main/java/br/com/connectpeople/security/SecurityConfig.java
@@ -1,7 +1,10 @@
-package br.com.connectpeople.api.security;
+package br.com.connectpeople.security;
 
 import br.com.connectpeople.user.domain.Role;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,12 +14,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableConfigurationProperties(OAuth2ClientProperties.class)
 public class SecurityConfig {
 
     @Value("${security.admin}")
@@ -55,7 +65,6 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 
 
     @Bean
