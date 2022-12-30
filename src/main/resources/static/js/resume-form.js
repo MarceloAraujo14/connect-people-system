cb8de1ebc67a19f35ae702e2167188ed347c4a68
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-12-30 18:13:21 -0300

    T-002 - feat: (interface): implement add superior academic

--- src/main/resources/static/js/resume-form.js
+++ src/main/resources/static/js/resume-form.js
@@ -9,7 +9,7 @@ function loadMonthAndYear() {
   monthSelect.forEach(month => {
     for (const monthName of months) {
       const option = document.createElement("option");
-      option.value = months.indexOf(monthName) +1;
+      option.value = months.indexOf(monthName) + 1;
       option.text = monthName;
       month.appendChild(option);
     }
@@ -35,10 +35,10 @@ let jobCount = 0;
 
 function addJobExp() {
 
-  if(jobCount == 3){
+  if (jobCount >= 3) {
     return;
   }
-  
+
   const job_xp = document.createDocumentFragment();
   const job = document.createElement('div');
   job.className = "job-experience";
@@ -56,25 +56,25 @@ function addJobExp() {
   </div>
   <br>
   <div class="row">
-    <div class="col">
+    <div class="col" id="startJob">
       <p>Entrada</p>
       <div class="row">
-          <div class="col-md-5">
-              <label for="month" class="form-label">Mês</label>
-              <select class="form-select month" id="startMonth" name="month"></select>
-            </div>
+        <div class="col-md-5">
+          <label for="month" class="form-label">Mês</label>
+          <select class="form-select month" id="startMonth" name="month"></select>
+        </div>
         <div class="col-md-4">
           <label for="year" class="form-label">Ano</label>
           <select class="form-select year" id="startYear" name="year"></select>
         </div>
         <div class="row-cols-1">
           <label class="form-check-label" for="isCurrentJob">Trabalho atual</label>
-          <input class="form-check-input" type="checkbox" id="isCurrentJob" name="isCurrentJob" >
+          <input class="form-check-input" onchange="currentJob(this)" type="checkbox" id="isCurrentJob" name="isCurrentJob" checked>
         </div>
       </div>
     </div>
   
-    <div class="col">
+    <div class="col" id="endJob">
       <p>Saída</p>
       <div class="row">
         <div class="col-md-5">
@@ -94,7 +94,7 @@ function addJobExp() {
     <textarea class="form-control" id="description" rows="3" name="description"></textarea>
   </div>
   <div class="del-xp-container">
-    <button type="button" class="btn-del-xp" onclick="deleteJobExp(this)">- Remover Experiência</button>
+    <button type="button" class="btn-del" onclick="deleteJobExp(this)">- Remover Experiência</button>
   </div>
   <div class="divider"></div>
   <br> </div>
@@ -105,25 +105,66 @@ function addJobExp() {
   element.append(job_xp);
   loadMonthAndYear();
   jobCount++;
-  if(jobCount == 3){
+  if (jobCount == 3) {
     document.getElementById("btn-add-xp").hidden = true;
+  }
+}
+
+function currentJob(checkbox){
+  if(checkbox.checked){
+      checkbox.parentElement.parentElement.parentElement.parentElement.querySelector("#endJob").innerHTML = 
+      `<p>Saída</p>
+      <div class="row">
+        <div class="col-md-5">
+          <label for="month" class="form-label">Mês</label>
+          <select class="form-select month" id="endMonth" name="month"></select>
+        </div>
+        <div class="col-md-4">
+          <label for="year" class="form-label">Ano</label>
+          <select class="form-select year" id="endYear" name="year"></select>
+        </div>
+      </div>
+    </div>`
+  }else {
+    checkbox.parentElement.parentElement.parentElement.parentElement.querySelector("#endJob").innerHTML = '';
   }
 }
 
 function deleteJobExp(element) {
   element.parentElement.parentElement.outerHTML = `<div id="job-xp-container"></div>`;
   jobCount--;
-  if(jobCount < 3){
+  if (jobCount < 3) {
     document.getElementById("btn-add-xp").hidden = false;
   }
 }
 
-function hasSuperior(){
-  var schooling = document.getElementById("schooling");
-  var superior = document.getElementById("superior-container");
-    if (schooling.value == 'SUPERIOR_INCOMPLETO'){
-      
-      superior.innerHTML = `<label>Curso Superior</label>
+let superiorCount = 0;
+const supIncomp = `
+<div id="superior">
+  <label>Curso Superior</label>
+  <div class="row" id="superior-child">
+    <div class="col-md-4">
+        <label for="college_name" class="form-label">Instituição</label>
+        <input type="text" class="form-control" id="college_name" name="college_name">
+    </div>
+    <div class="col-md-4">
+        <label for="college_course" class="form-label">Curso</label>
+        <input type="text" class="form-control" id="college_course" name="college_course">
+    </div>
+    <div class="col-md-4">
+        <label for="college_step" class="form-label">Situação</label>
+        <select id="college_step" class="form-select option" name="college_step">
+            <option value="CURSANDO">Cursando</option>
+            <option value="TRANCADO">Trancado ou Interrompido</option>
+        </select>
+    </div>
+  </div>
+  <br>
+</div>
+`;
+const supIncompRem = `
+  <div class="col" id="superior-child">
+    <div class="row">
       <div class="col-md-4">
           <label for="college_name" class="form-label">Instituição</label>
           <input type="text" class="form-control" id="college_name" name="college_name">
@@ -138,9 +179,38 @@ function hasSuperior(){
               <option value="CURSANDO">Cursando</option>
               <option value="TRANCADO">Trancado ou Interrompido</option>
           </select>
-      </div>`
-    }else if(schooling.value == 'SUPERIOR_COMPLETO'){
-      superior.innerHTML = `<label>Curso Superior</label>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <button type="button" class="btn-del" onclick="deleteSuperior(this)"> - remove </button>
+      </div>
+    </div>
+  </div>
+`;
+const supComp = `
+<div id='superior' class="superior">
+  <label>Curso Superior</label>
+  <div class="row" id="superior-child">
+    <div class="col-md-4">
+        <label for="college_name" class="form-label">Instituição</label>
+        <input type="text" class="form-control" id="college_name" name="college_name">
+    </div>
+    <div class="col-md-4">
+        <label for="college_course" class="form-label">Curso</label>
+        <input type="text" class="form-control" id="college_course" name="college_course">
+    </div>
+    <div class="col-md-3">
+        <label for="college_year" class="form-label">Ano de Conclusão</label>
+        <select id="college_year" class="form-select option" name="year"></select>
+    </div>
+  </div>
+  <br>
+</div>
+`;
+const supCompRem = ` 
+  <div class="row" id="superior-child">
+    <div class="row">
       <div class="col-md-4">
           <label for="college_name" class="form-label">Instituição</label>
           <input type="text" class="form-control" id="college_name" name="college_name">
@@ -152,15 +222,71 @@ function hasSuperior(){
       <div class="col-md-3">
           <label for="college_year" class="form-label">Ano de Conclusão</label>
           <select id="college_year" class="form-select option" name="year"></select>
-      </div>`
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <button type="button" class="btn-del" onclick="deleteSuperior(this)"> - remove </button>
+      </div>
+    </div>
+  </div>
+`;
 
-      loadMonthAndYear();
-    }else {
-      superior.innerHTML = '';
+function addSuperior(element) {
+
+  if(superiorCount >=3){
+    return;
+  }
+
+  var superior = document.getElementById("superior-container");
+  var schooling = document.getElementById("schooling");
+
+  if (schooling.value == 'SUPERIOR_INCOMPLETO') {
+    if (superiorCount == 0){
+      superior.innerHTML = supIncomp;
+    } else {
+      element.outerHTML = supIncompRem;
     }
+    
+    
+  } else if (schooling.value == 'SUPERIOR_COMPLETO') {
+    if(superiorCount == 0){
+      superior.innerHTML = supComp;
+    }else{
+      superior.innerHTML = supCompRem;
+    }
+    
+  } else {
+    superior.innerHTML = '';
+    return;
+  } 
+
+  
+  const btn_sup_container = document.createElement("div");
+  btn_sup_container.className = "btn-sup-container";
+  btn_sup_container.innerHTML = '<button type="button" class="btn-add" onclick="addSuperior(this)">Clique Aqui</button>';
+  superior.append(btn_sup_container);
+
+  loadMonthAndYear();
+  superiorCount++;
+  console.log(superiorCount)
 }
 
-async function buildResumeFromJson(){
+function deleteSuperior(element) {
+  element.parentElement.parentElement.parentElement.outerHTML = ``;
+  superiorCount--;
+  console.log(superiorCount)
+}
+
+function resetCount(){
+  superiorCount = 0;
+  console.log(superiorCount);
+}
+
+
+
+
+async function buildResumeFromJson() {
   // var resume = await fetch('./json/ResumeRequest.json').then(response => response.json());
   var resume = await fetch('../static/json/ResumeRequest.json').then(response => response.json());
 
@@ -183,7 +309,7 @@ async function buildResumeFromJson(){
 
   var count = jobExperiences.length;
 
-  for(let i = 0; i < count; i++){
+  for (let i = 0; i < count; i++) {
     resume['jobExperiences'][i] = {};
     resume.jobExperiences[i]['title'] = jobExperiences[i].querySelector('#title').value;
     resume.jobExperiences[i]['company'] = jobExperiences[i].querySelector('#company').value;
@@ -198,30 +324,30 @@ async function buildResumeFromJson(){
   return resume;
 }
 
-async function sendResume(){
+async function sendResume() {
 
   const resume = buildResumeFromJson();
 
   const response = await post('http://localhost:80/api/resume', resume);
-  
+
   if (response.status >= 200 && response.status <= 299) {
     document.querySelector('.success-message').innerHTML = "<p>Currículo Cadastrado com sucesso!</p>";
   }
-  
+
   if (response.status >= 400 && response.status <= 499) {
     document.querySelector('.success-message').innerHTML = "<p>Currículo Cadastrado com sucesso!</p>";
-  } 
+  }
 
 }
 
 //api functions
-async function post(url, obj){
+async function post(url, obj) {
   const response = await fetch(url, {
-  method: 'POST',
-  body: JSON.stringify(obj),
-  headers: {'Content-type': 'application/json'}
+    method: 'POST',
+    body: JSON.stringify(obj),
+    headers: { 'Content-type': 'application/json' }
   })
-  .then(res => res);
+    .then(res => res);
 
   console.log(response)
 
