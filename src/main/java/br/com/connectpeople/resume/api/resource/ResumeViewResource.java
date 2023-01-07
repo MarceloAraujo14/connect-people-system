48138a45796eb19a649e554e630f6aadb6268368
marceloaraujo14 <mbaraujo1406@gmail.com>
2023-01-07 00:03:55 -0300

    feat/CPC-005 - feat: implement logout button

--- src/main/java/br/com/connectpeople/resume/api/resource/ResumeViewResource.java
+++ src/main/java/br/com/connectpeople/resume/api/resource/ResumeViewResource.java
@@ -1,6 +1,9 @@
 package br.com.connectpeople.resume.api.resource;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,13 +12,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ResumeViewResource {
 
     @GetMapping
-    public String home(){
+    public String home(Authentication authentication, Model model){
+        model.addAttribute("username", getUsername(authentication));
         return "resume-form";
     }
 
     @GetMapping("/resume")
-    public String resume(){
+    public String resume(Authentication authentication, Model model){
+        model.addAttribute("username", getUsername(authentication));
         return "resume-form";
+    }
+
+    private String getUsername(Authentication authentication){
+        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+        if(user.getAttributes().containsKey("localizedFirstName")){
+            return user.getAttribute("localizedFirstName") + " " + user.getAttribute("localizedLastName");
+        }
+        if(user.getAttributes().containsKey("given_name")){
+            return user.getAttribute("given_name");
+        }
+        return user.getName();
     }
 
 }
