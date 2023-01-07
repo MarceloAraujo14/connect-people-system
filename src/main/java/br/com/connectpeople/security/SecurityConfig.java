ccf58f1e9b117c65c8b762d2149260d377773006
marceloaraujo14 <mbaraujo1406@gmail.com>
2023-01-06 22:14:08 -0300

    feat/CPC-004 - feat: implement linkedin oauth2 authentication

--- src/main/java/br/com/connectpeople/security/SecurityConfig.java
+++ src/main/java/br/com/connectpeople/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2Clien
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,14 +15,28 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.endpoint.DefaultMapOAuth2AccessTokenResponseConverter;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
 @EnableConfigurationProperties(OAuth2ClientProperties.class)
 public class SecurityConfig {
+
+    public static final String LOGIN = "/login";
 
     @Value("${security.admin}")
     private String pass;
@@ -36,29 +52,47 @@ public class SecurityConfig {
 //                .cors().disable()
 //                .csrf().disable()
 //                .authorizeHttpRequests().anyRequest().permitAll();
-                .csrf().disable()
-                .cors().disable()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/static/**", "/css/**", "/js/**", "/img/**", "/actuator/health").permitAll()
+                        .requestMatchers(LOGIN, "/static/**", "/css/**", "/js/**", "/img/**", "/actuator/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .headers(headers -> headers.frameOptions().sameOrigin())
                 .oauth2Login(oauth -> oauth
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/", true)
-                        .failureUrl("/login?error=true"))
+                        .loginPage(LOGIN)
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error=true")
+                        .tokenEndpoint(
+                                token -> {
+                                    var defaultMapConverter = new DefaultMapOAuth2AccessTokenResponseConverter();
+                                    Converter<Map<String, Object>, OAuth2AccessTokenResponse> linkedinMapConverter = tokenResponse -> {
+                                        var withTokenType = new HashMap<>(tokenResponse);
+                                        withTokenType.put(OAuth2ParameterNames.TOKEN_TYPE, OAuth2AccessToken.TokenType.BEARER.getValue());
+                                        return defaultMapConverter.convert(withTokenType);
+                                    };
+
+                                    var httpConverter = new OAuth2AccessTokenResponseHttpMessageConverter();
+                                    httpConverter.setAccessTokenResponseConverter(linkedinMapConverter);
+
+                                    var restOperations = new RestTemplate(List.of(new FormHttpMessageConverter(), httpConverter));
+                                    restOperations.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+                                    var client = new DefaultAuthorizationCodeTokenResponseClient();
+                                    client.setRestOperations(restOperations);
+
+                                    token.accessTokenResponseClient(client);
+                                }
+                        ))
                 .formLogin(login -> login
-                        .loginPage("/login")
+                        .loginPage(LOGIN)
                         .usernameParameter("email")
                         .passwordParameter("password")
                         .defaultSuccessUrl("/", true)
-                        .loginProcessingUrl("/login")
+                        .loginProcessingUrl(LOGIN)
                         .failureForwardUrl("/login?error=true")
                         .permitAll())
                 .logout(
                         logout -> logout
                                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
-                                .logoutSuccessUrl("/login").permitAll()
+                                .logoutSuccessUrl(LOGIN).permitAll()
                 );
 
         return http.build();
